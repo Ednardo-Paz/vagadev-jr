7da66fed39191225ae4c31f297a0b7f8075d5cb0
Ednardo Paz <eds_paz@hotmail.com>
2022-06-18 14:23:53 -0300

    Init: IconhamburguerSvg

---
+++ vagadev/src/componentsGeneral/IconHamburguerSvg.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const IconHamburguer = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="24"
+      version="1.1"
+      viewBox="0 0 30 24"
+    >
+      <g fill="#000" transform="translate(-382 -48)">
+        <rect
+          width="30"
+          height="3"
+          data-name="Retângulo 2"
+          rx="1.5"
+          transform="translate(382 48)"
+        ></rect>
+        <rect
+          width="30"
+          height="3"
+          data-name="Retângulo 3"
+          rx="1.5"
+          transform="translate(382 58.451)"
+        ></rect>
+        <rect
+          width="22"
+          height="3"
+          data-name="Retângulo 4"
+          rx="1.5"
+          transform="translate(382 69)"
+        ></rect>
+      </g>
+    </svg>
+  );
+};
+
+export default IconHamburguer;

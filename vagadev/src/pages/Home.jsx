a5e47261d12cd1aa0196bb7a74c7f0f78a274807
Ednardo Paz <eds_paz@hotmail.com>
2022-06-18 22:03:22 -0300

    Feat: Getting SectionBanner

--- vagadev/src/pages/Home.jsx
+++ vagadev/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Menu from '../components/Menu';
+import SectionBanner from '../components/SectionBanner';
 
 const Home = () => {
-  return <Menu />;
+  return (
+    <div className="container">
+      <Menu />
+      <SectionBanner />
+    </div>
+  );
 };
 
 export default Home;

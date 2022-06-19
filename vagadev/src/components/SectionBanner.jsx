743a38bda7b48a74b6425f5dc65f2ebfc325d466
Ednardo Paz <eds_paz@hotmail.com>
2022-06-19 02:27:08 -0300

    Feat: getting SliderNav

--- vagadev/src/components/SectionBanner.jsx
+++ vagadev/src/components/SectionBanner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './SectionBanner.module.css';
 import banner from '../assets/img/principal_banner_desktop.jpg';
+import SliderNav from './SliderNav';
 
 const SectionBanner = () => {
   return (
     <section className={styles.sectionContainer}>
       <img className={styles.imgBanner} src={banner} alt="" />
+      <SliderNav />
     </section>
   );
 };

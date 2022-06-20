d87af9c81129e364d733bc8988e9973278d59313
Ednardo Paz <eds_paz@hotmail.com>
2022-06-20 18:01:17 -0300

    Feat: getting sectionContainer and ReacttangleMiddleBanner

--- vagadev/src/components/SectionMiddleBanner.jsx
+++ vagadev/src/components/SectionMiddleBanner.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import styles from './SectionMiddleBanner.module.css';
 import imgZelda from '../assets/img/zelda_banner.jpg';
 import imgSekiro from '../assets/img/sekiro_banner.jpg';
+import RectangleMiddlebanner from '../componentsGeneral/RectangleMiddlebanner';
 const SectionMiddleBanner = () => {
   return (
-    <section className={styles.middleBanner}>
+    <section className={`${styles.middleBanner} sectionContainer`}>
       <div>
         <img className={styles.middleBannerImg} src={imgZelda} alt="" />
+        <RectangleMiddlebanner
+          text={'The Legend of Zelda - Breath of th wild'}
+        />
       </div>
       <div>
         <img className={styles.middleBannerImg} src={imgSekiro} alt="" />
+        <RectangleMiddlebanner text={'SEKIRO - Shadows die twice'} />
       </div>
     </section>
   );

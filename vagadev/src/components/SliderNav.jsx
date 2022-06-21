56d02e4af9517c8141e0a0fe0426b666ccfeff97
Ednardo Paz <eds_paz@hotmail.com>
2022-06-21 17:41:24 -0300

    Adjust: getting buttons

--- vagadev/src/components/SliderNav.jsx
+++ vagadev/src/components/SliderNav.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+import SlideNextButton from '../componentsGeneral/ButtonNext';
+import SlidePrevButton from '../componentsGeneral/ButtonPrev';
 import IconLeftSvg from '../componentsGeneral/IconLeftSvg';
 import IconRightSvg from '../componentsGeneral/IconRightSvg';
 import styles from './SliderNav.module.css';
 
 const SliderNav = () => {
   return (
-    <div className={styles.slider}>
-      <p className={styles.sliderText}>MORTAL KOMBAT</p>
-      <p className={styles.pagination}>
-        <span className={styles.paginationText}>1/2</span>
-        <div className={styles.iconsArrows}>
-          <div className={styles.iconRight}>
-            <IconRightSvg color={'#fff'} width={'10px'} height={'17px'} />
-          </div>
-          <div className={styles.iconLeft}>
-            <IconLeftSvg color={'#fff'} width={'10px'} height={'17px'} />
+    <>
+      <div className={styles.slider}>
+        <p className={styles.sliderText}>MORTAL KOMBAT</p>
+        <div className={styles.pagination}>
+          <span className={styles.paginationText}>1/2</span>
+          <div className={styles.iconsArrows}>
+            <div className={styles.iconRight}>
+              <SlideNextButton
+                element={
+                  <IconRightSvg color={'#fff'} width={'10px'} height={'17px'} />
+                }
+              />
+            </div>
+            <div className={styles.iconLeft}>
+              <SlidePrevButton
+                element={
+                  <IconLeftSvg color={'#fff'} width={'10px'} height={'17px'} />
+                }
+              />
+            </div>
           </div>
         </div>
-      </p>
-    </div>
+      </div>
+    </>
   );
 };
 

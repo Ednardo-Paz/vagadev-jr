b47937a467600536dbecea820b608cb47f4d2da9
Ednardo Paz <eds_paz@hotmail.com>
2022-06-21 00:02:32 -0300

    Feat: Getting Card in SectionCard

--- vagadev/src/components/sections/SectionCard.jsx
+++ vagadev/src/components/sections/SectionCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import TitleSctionWhite from '../../componentsGeneral/TitleSctionWhite';
+import Card from '../Card';
 import styles from './SectionCard.module.css';
+import { jogos } from '../../obj';
 
 const SectionCard = () => {
+  console.log(jogos);
   return (
     <div className={`${styles.sectionCard} sectionContainer`}>
       <TitleSctionWhite text={'Produtos em destaque'} />
+      <div className={styles.allCards}>
+        {jogos.map((jogo) => (
+          <Card
+            key={jogo.id}
+            img={jogo.img}
+            title={jogo.title}
+            price={jogo.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };

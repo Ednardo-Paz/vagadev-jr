3f08b7640746c52c11bc0d27582200e11fd2a7af
Ednardo Paz <eds_paz@hotmail.com>
2022-06-20 22:12:01 -0300

    Freat: getting img

--- vagadev/src/componentsGeneral/TitleSctionWhite.jsx
+++ vagadev/src/componentsGeneral/TitleSctionWhite.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import styles from './TitleSectionWhite.module.css';
+import triangle from '../assets/img/h1triang.png';
 const TitleSctionWhite = ({ text }) => {
-  return <h1 className={styles.titleSctionWhite}>{text}</h1>;
+  return (
+    <div className={styles.titleSctionWhite}>
+      <div>
+        <img className={styles.imgTitle} src={triangle} alt="triangle" />
+      </div>
+      <h1 className={styles.titleSctionWhiteH1}>{text}</h1>
+    </div>
+  );
 };
 
 export default TitleSctionWhite;

03652708cf138755641ab3aac3d866819b0079d0
Ednardo Paz <eds_paz@hotmail.com>
2022-06-21 00:00:37 -0300

    Init: Obj for interation

---
+++ vagadev/src/obj.js
@@ -0,0 +1,24 @@
+import imgOutriders from './assets/img/product-outriders.jpg';
+import imgCyber from './assets/img/product-cyberpunk2077.jpg';
+import imgDonkey from './assets/img/product-donkey-kong-country-tropical-freeze.jpg';
+
+export const jogos = [
+  {
+    id: 0,
+    img: imgOutriders,
+    price: 200.0,
+    title: 'Outriders',
+  },
+  {
+    id: 1,
+    img: imgCyber,
+    price: 200.0,
+    title: 'CYBERPUNK 2077',
+  },
+  {
+    id: 2,
+    img: imgDonkey,
+    price: 200.0,
+    title: 'Donkey Kong Countru Tropical Freeze',
+  },
+];

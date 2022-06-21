47f14182e5f763ad5018eaf411ba8bae831358ea
Ednardo Paz <eds_paz@hotmail.com>
2022-06-21 17:34:23 -0300

    Adjust: Add new obj

--- vagadev/src/obj.js
+++ vagadev/src/obj.js
@@ -1,10 +1,12 @@
 import imgOutriders from './assets/img/product-outriders.jpg';
 import imgCyber from './assets/img/product-cyberpunk2077.jpg';
 import imgDonkey from './assets/img/product-donkey-kong-country-tropical-freeze.jpg';
+import banner1 from '../src/assets/img/principal_banner_desktop.jpg';
+import banner2 from '../src/assets/img/principal_banner_desktop_02.jpg';
 
 export const jogos = [
   {
-    id: 0,
+    id: 56,
     img: imgOutriders,
     price: 200.0,
     title: 'Outriders',
@@ -16,9 +18,27 @@ export const jogos = [
     title: 'CYBERPUNK 2077',
   },
   {
-    id: 2,
+    id: 27,
     img: imgDonkey,
     price: 200.0,
     title: 'Donkey Kong Countru Tropical Freeze',
+  },
+];
+
+export const banners = [
+  {
+    id: 2,
+    img: banner1,
+    title: 'MORTAL KOMBAT',
+    price: 299,
+    description:
+      'Mortal Kombat X combina uma apresentação cinemática única com uma jogabilidade totalmente nova. Os jogadores podem escolher pela primeira vez diversas variantes de cada personagem, afetando tanto a estratégia como o estilo de luta.',
+  },
+  {
+    id: 5,
+    img: banner2,
+    title: '',
+    price: '',
+    description: '',
   },
 ];

3fdfbee8c31107394f675d5d8a8893657226c0c9
Ednardo Paz <eds_paz@hotmail.com>
2022-06-20 21:56:14 -0300

    Init: Title section white

---
+++ vagadev/src/componentsGeneral/TitleSctionWhite.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './TitleSectionWhite.module.css';
+const TitleSctionWhite = ({ text }) => {
+  return <h1 className={styles.titleSctionWhite}>{text}</h1>;
+};
+
+export default TitleSctionWhite;

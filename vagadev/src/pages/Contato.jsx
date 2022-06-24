c1521a7e2559ecfcbad22df02daac3437b8b317a
Ednardo Paz <eds_paz@hotmail.com>
2022-06-24 12:59:31 -0300

    Adjust: Contact

--- vagadev/src/pages/Contato.jsx
+++ vagadev/src/pages/Contato.jsx
@@ -4,7 +4,13 @@ import styles from './Contato.module.css';
 const Contato = () => {
   return (
     <>
-      <div className={`${styles.contato} sectionContainer`}></div>
+      <div className={`${styles.contato} sectionContainer`}>
+        <div className={styles.contatoSction}>
+          <div>
+            <h1 className={styles.contatoH1}>Entre em Contato...</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
